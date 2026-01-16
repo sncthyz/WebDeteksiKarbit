93e3937a1d23777d772e0bcb368f8ec3c0c03a6a
sncthyz <andikacahyarahman123@gmail.com>
2026-01-16 17:47:13 +0700

    kesembilan

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import bgvideo from "../public/Gamings Background.mp4";
 
 export default function App(){
 const [nama,setNama] = useState('');
@@ -9,36 +9,36 @@ const [output,setOutput] = useState<Record<string, number>>({});
       <div className="p-4 place-items-center text-center shadow-lg bg-neutral-950 md:w-140 w-110 rounded-3xl mt-5 mb-5 mx-auto ">
         <h1 className="text-yellow-300 md:text-3xl text-2xl font-bold">Kamu {nama} terdeteksi 80% Karbit</h1>
       <p className="text-yellow-300 md:text-xl text-lg mb-3 font-semibold">Pergi sana bit karbit </p>
-           <img src="./src/assets/gif1.gif" className="mx-auto size-55" alt="" />
+           <img src="../public/gif1.gif" className="mx-auto size-55" alt="" />
       </div>,
 
       <div className="p-4 place-items-center text-center shadow-lg bg-neutral-950 w-140 rounded-3xl mt-5 mb-5 mx-auto ">
       <h1 className="text-yellow-300 text-3xl font-bold">Kamu {nama} terdeteksi 100% Karbit</h1>
       <p className="text-yellow-300 text-xl mb-3 font-semibold">Karbit bet jir </p>
-           <img src="./src/assets/gambar2.png" className="mx-auto size-55" alt="" />
+           <img src="../public/gambar2.png" className="mx-auto size-55" alt="" />
       </div>,
       <div className="p-4 place-items-center text-center shadow-lg bg-neutral-950 w-140 rounded-3xl mt-5 mb-5 mx-auto ">
       <h1 className="text-yellow-300 text-3xl font-bold">Kamu {nama} terdeteksi 25% Karbit</h1>
       <p className="text-yellow-300 text-xl mb-3 font-semibold">Aman lah ya </p>
-           <img src="./src/assets/Hai Karbit GIF.gif" className="mx-auto size-55" alt="" />
+           <img src="../public/Hai Karbit GIF.gif" className="mx-auto size-55" alt="" />
 
       </div>,
        <div className="p-4 place-items-center text-center shadow-lg bg-neutral-950 w-140 rounded-3xl mt-5 mb-5 mx-auto ">
       <h1 className="text-yellow-300 text-3xl font-bold">Kamu {nama} terdeteksi 60% Karbit</h1>
       <p className="text-yellow-300 text-xl mb-3 font-semibold">Mulai dah karbitnya </p>
-           <img src="./src/assets/Andreasruzuu GIF.gif" className="mx-auto size-55" alt="" />
+           <img src="../public/Andreasruzuu GIF.gif" className="mx-auto size-55" alt="" />
 
       </div>,
        <div className="p-4 place-items-center text-center shadow-lg bg-neutral-950 w-140 rounded-3xl mt-5 mb-5 mx-auto ">
       <h1 className="text-yellow-300 text-3xl font-bold">Kamu {nama} terdeteksi 73% Karbit</h1>
       <p className="text-yellow-300 text-xl mb-3 font-semibold">Duh kebangetan ini mah </p>
-           <img src="./src/assets/gambar3.png" className="mx-auto size-55" alt="" />
+           <img src="../public/gambar3.png" className="mx-auto size-55" alt="" />
 
       </div>,
        <div className="p-4 place-items-center text-center shadow-lg bg-neutral-950 w-140 rounded-3xl mt-5 mb-5 mx-auto ">
       <h1 className="text-yellow-300 text-3xl font-bold">Kamu {nama} terdeteksi 0% Karbit</h1>
       <p className="text-yellow-300 text-xl mb-3 font-semibold"> Selamat anda bukan seorang karbit </p>
-           <img src="./src/assets/Happy Cheer GIF.gif" className="mx-auto size-55" alt="" />
+           <img src="../public/Happy Cheer GIF.gif" className="mx-auto size-55" alt="" />
 
       </div>,
 
@@ -63,8 +63,8 @@ const [output,setOutput] = useState<Record<string, number>>({});
   return (
     <>
     <div className="relative min-h-screen overflow-hidden">
-      <video autoPlay loop muted playsInline className="fixed inset-0 w-full h-full object-cover ">
-        <source  src="{`${import.meta.env.BASE_URL} Gamings Background.mp4`}"/>
+      <video autoPlay loop muted className="fixed inset-0 w-full h-full object-cover ">
+        <source  src={bgvideo}/>
       </video>
       <div className="relative z-10 text-center place-content-center justify-center items-center min-h-screen">
         <div className="">
